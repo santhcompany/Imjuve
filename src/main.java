884272e1777d073a73370c594a54ded07724aa2f
EdRios <edwin.rios197@gmail.com>
2019-04-16 13:57:26 -0700

    cambio23123

--- src/main.java
+++ src/main.java
@@ -5,7 +5,7 @@ public class main {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		"kaka"
+		          System.out.println("Pinche cambio qlo");
 	}
 	
 }
