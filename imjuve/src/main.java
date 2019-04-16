ed411107cbf03728fa74093898d80aaf84b542e3
Luis Sebastián <cvas_2_03@hotmail.com>
2019-04-16 14:56:47 -0700

    Update main.java

--- imjuve/src/main.java
+++ imjuve/src/main.java
@@ -5,7 +5,7 @@ public class main {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		System.out.println("Los putos cambios");
+		System.out.println("Ya le ando agarrando el rollo");
 	}
 	
 }
